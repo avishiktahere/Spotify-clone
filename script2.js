761362daf9bfceff1ad6fd39177f5755722d5a92
avishiktahere <157952001+avishiktahere@users.noreply.github.com>
2025-07-04 23:32:55 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,6 @@
+document.querySelector('.toggle-password').addEventListener('click', function () {
+    const passwordField = document.getElementById('password');
+    const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordField.setAttribute('type', type);
+    this.textContent = this.textContent === '👁️' ? '🔒' : '👁️';
+});
